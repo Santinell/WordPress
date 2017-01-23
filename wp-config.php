bfe0a2b765d657b320ce34f5b823772f1ad5df1b
Pavel Evdokimov <picnixer@gmail.com>
2017-01-23 11:41:21 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,21 +1,24 @@
 <?php
-$vars = array('DB_NAME',
-'DB_USER',
-'DB_PASSWORD',
-'DB_HOST',
-'DB_CHARSET',
-'DB_COLLATE',
-'AUTH_KEY',
-'SECURE_AUTH_KEY',
-'LOGGED_IN_KEY',
-'NONCE_KEY',
-'AUTH_SALT',
-'SECURE_AUTH_SALT',
-'LOGGED_IN_SALT',
-'NONCE_SALT',
-'WP_DEBUG');
-foreach ($vars as $var) {
-  define($var, getenv($var));
+$vars = [
+  'DB_NAME' => 'wordpress',
+  'DB_USER' => 'root',
+  'DB_PASSWORD' => '',
+  'DB_HOST' => 'mysql-master',
+  'DB_CHARSET' => 'utf8',
+  'DB_COLLATE' => '',
+  'AUTH_KEY' => 'put your unique phrase here',
+  'SECURE_AUTH_KEY' => 'put your unique phrase here',
+  'LOGGED_IN_KEY' => 'put your unique phrase here',
+  'NONCE_KEY' => 'put your unique phrase here',
+  'AUTH_SALT' => 'put your unique phrase here',
+  'SECURE_AUTH_SALT' => 'put your unique phrase here',
+  'LOGGED_IN_SALT' => 'put your unique phrase here',
+  'NONCE_SALT' => 'put your unique phrase here',
+  'WP_DEBUG' => false
+];
+foreach ($vars as $var => $default) {
+  $env = getenv($var);
+  define($var, $env === false ? $default : $env);
 }
 
 $table_prefix  = 'wp_';
